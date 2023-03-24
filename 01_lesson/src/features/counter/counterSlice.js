9309f10d96afac188dc43a7cd3c2f66f39cfc5c9
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-24 11:57:19 +0100

    Fix: typos in createSlice

--- 01_lesson/src/features/counter/counterSlice.js
+++ 01_lesson/src/features/counter/counterSlice.js
@@ -5,14 +5,14 @@ const initialState = {
 }
 
 export const counterSlice = createSlice({
-    name: 'counterSlice',
+    name: 'counter',
     initialState,
     reducers: {
         increment: (state) => {
             state.count += 1
         },
         decrement: (state) => {
-            state.count = 1
+            state.count -= 1
         }
     }
 })
