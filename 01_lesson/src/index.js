57e71c9114e6818c8a7c3d279fe49f125adbb746
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-24 10:57:37 +0100

    Style: css classes to style counter app

--- 01_lesson/src/index.js
+++ 01_lesson/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
+   <React.StrictMode>
+     <Provider store={store}>
+        <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+   </React.StrictMode>,
+    document.getElementById('root')
+)
