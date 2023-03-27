bbc75e5ee5288f5404203d7b3789bd635ab93dcc
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-27 22:32:07 +0100

    mass commit

--- 02_lesson_starter/src/app/store.js
+++ 02_lesson_starter/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
 
-const store = configureStore({
+export const store = configureStore({
     reducer: {
-        posts: postsReducer
+        posts: postsReducer,
+        users: usersReducer
     }
 })
--- 02_lesson_starter/src/features/posts/postsSlice.js
+++ 02_lesson_starter/src/features/posts/postsSlice.js
@@ -1,18 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'Redux toolkit projects', content: 'Redux toolkit is a powerful tool. I love it'},
-    { id: '2', title: 'Learning Slices', content: 'This is a slice'}
+    { id: '1', title: 'Redux toolkit projects', content: 'Redux toolkit is a powerful tool. I love it', date: sub(new Date(), { minutes: 10}).toISOString() },
+    { id: '2', title: 'Learning Slices', content: 'This is a slice', date: sub(new Date(), { minutes: 10}).toISOString() }
 ]
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {
-
+    reducers: {
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        date: new Date().toISOString,
+                        userId
+                    }
+                }
+            }
+        }
     }
 })
 
-export const selectAllPosts = state =>state.posts
+export const selectAllPosts = (state) => state.posts
+
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
