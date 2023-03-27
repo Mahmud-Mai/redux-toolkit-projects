d174ea8de4d1d11bf6b31ed0eeb3a6ebb860f5ef
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-27 23:01:31 +0100

    Feat pass reaction emojis to posts lists

--- 02_lesson_starter/src/features/posts/PostsList.js
+++ 02_lesson_starter/src/features/posts/PostsList.js
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux"
 import { selectAllPosts } from "./postsSlice"
 import PostAuthor from "./PostAuthor"
 import TimeAgo from "./TimeAgo"
+import ReactionButtons from "./ReactionButtons"
 
  const PostsList = () => {
     const posts = useSelector(selectAllPosts)
@@ -16,6 +17,7 @@ import TimeAgo from "./TimeAgo"
               <PostAuthor userId={post.userId} />
               <TimeAgo timestamp={post.date}/>
             </p>
+            <ReactionButtons post={post} />
         </article>
     ))
 
