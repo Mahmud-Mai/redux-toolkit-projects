a38784f92e2a087a9a312624e484241373b1d008
Mahmud Mai <mahmud.mahmudx00@gmail.com>
2023-03-28 10:07:06 +0100

    Feat: modify postsSlice to fetch users from jsonplaceholder

--- 03_lesson_starter/src/features/posts/postsSlice.js
+++ 03_lesson_starter/src/features/posts/postsSlice.js
@@ -68,10 +68,10 @@ const postsSlice = createSlice({
                     post.date = sub(new Date(), { minutes: min++ }).toISOString();
                     post.reactions = {
                         thumbsUp: 0,
-                        wow: 0,
-                        heart: 0,
-                        rocket: 0,
-                        coffee: 0
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
                     }
                     return post;
                 });
